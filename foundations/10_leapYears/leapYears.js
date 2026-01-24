5c74dec5c0d5fe9429aa8adb4b2b42bee4027772
jrw <197988939+Jrwick@users.noreply.github.com>
2026-01-24 14:58:13 +0000

    Complete exercises 06 thru 11 inclusive

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,23 @@
-const leapYears = function() {
-
+/*
+const leapYears = function (year) {
+  if (year % 4 == 0) {
+    if (year % 100 == 0) {
+      if (year % 400 == 0) {
+        return true;
+      }
+      return false;
+    }
+    return true;
+  }
+  return false;
 };
+*/
+
+const leapYears = function (year) {
+  return year % 400 == 0 ? true : year % 4 == 0 && year % 100 != 0;
+};
+
+//700 1900
 
 // Do not edit below this line
 module.exports = leapYears;
--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+//°C = (°F - 32) ÷ (9/5)
+const convertToCelsius = function (f) {
+  return Math.round(((f - 32) / (9 / 5)) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+//°F = °C * 9/5 + 32
+const convertToFahrenheit = function (c) {
+  return Math.round(((c * 9) / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
